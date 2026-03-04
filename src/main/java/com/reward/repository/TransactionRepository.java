a8465f2e4cc15fd9a387e717d08fe7a8045570d9
jatin0002 <pratapjatin999@gmail.com>
2026-03-04 18:12:07 +0530

    feat: add JPA repositories for Customer and Transaction entities

---
+++ src/main/java/com/reward/repository/TransactionRepository.java
@@ -0,0 +1,12 @@
+package com.reward.repository;
+
+import com.reward.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
+}
